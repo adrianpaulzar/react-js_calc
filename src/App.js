f679cfe5e87c7ad21d17f2d59d7c49b351617d44
adrianpaulzar <Adrianpaulzarate@gmail.com>
2023-08-25 22:27:06 +0800

    react-clean
    cleaned react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [num1, setNum1] = useState('');
+  const [num2, setNum2] = useState('');
+  const [operation, setOperation] = useState('add');
+
+  const calculate = () => {
+    const parsedNum1 = parseFloat(num1);
+    const parsedNum2 = parseFloat(num2);
+
+    if (isNaN(parsedNum1) || isNaN(parsedNum2)) {
+      return 'Invalid input';
+    }
+
+    switch (operation) {
+      case 'add':
+        return parsedNum1 + parsedNum2;
+      case 'subtract':
+        return parsedNum1 - parsedNum2;
+      case 'multiply':
+        return parsedNum1 * parsedNum2;
+      case 'divide':
+        return parsedNum2 !== 0 ? parsedNum1 / parsedNum2 : 'Cannot divide by zero';
+      default:
+        return '';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="calculator">
+      <h1>Calculator</h1>
+      <input type="text" placeholder="Enter number 1" value={num1} onChange={e => setNum1(e.target.value)} />
+      <input type="text" placeholder="Enter number 2" value={num2} onChange={e => setNum2(e.target.value)} />
+      <div className="buttons">
+        {['add', 'subtract', 'multiply', 'divide'].map(op => (
+          <button key={op} onClick={() => setOperation(op)}>{op.charAt(0).toUpperCase() + op.slice(1)}</button>
+        ))}
+      </div>
+      <h2>Result: {calculate()}</h2>
     </div>
   );
 }
